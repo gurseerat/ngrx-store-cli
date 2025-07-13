c88a9a31f5b015228e3987a92df2274dc30e24f3
gurseerat <me.gurseerat11@gmail.com>
2025-07-13 07:01:26 +0530

    Fixed schema

--- copy-template.js
+++ copy-template.js
@@ -2,4 +2,6 @@ import pkg from 'fs-extra';
 const {copySync} = pkg;
 
 copySync('src/schematics/store/files', 'dist/schematics/store/files', { overwrite: true });
+copySync('src/schematics/store/schema.json', 'dist/schematics/store/schema.json', { overwrite: true });
+
 console.log('Template files copied.');
